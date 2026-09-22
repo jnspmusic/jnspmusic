6f334d84b5ffcdfdd179bd16c317c89fb962c3cc
jnspmusic <jnsp@jnspmusic.com>
2026-09-22 18:57:43 +0200

    Feature vintage radio cassette artwork in artist section

--- src/App.tsx
+++ src/App.tsx
@@ -374,7 +374,7 @@ function App() {
             </a>
           </div>
           <div className="artist-visual">
-            <img src="/media/jnsp-turntable.webp" alt="JNSP vinyl turntable" loading="lazy" />
+            <img src="/media/jnsp-radio-cassette.webp" alt="Vintage JNSP radio cassette recorder" loading="lazy" />
             <div className="artist-stamp"><Radio size={28} /><span>INDEPENDENT<br />ARTIST</span></div>
           </div>
         </section>
